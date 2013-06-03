dda6e2ac95efb4f78e432ae5ce502ff26cf7017d
wind <aszx4510@yahoo.com.tw>
2013-06-04 00:38:24 +0800

    Add getId() and other words By.wind

--- PokerTmp/src/main/java/com/weigreen/poler/Card.java
+++ PokerTmp/src/main/java/com/weigreen/poler/Card.java
@@ -7,12 +7,24 @@ public class Card
 {
     private short Id;
 
+    public short getId()
+    {
+        return Id;
+    }
+
+    /**
+     * @return the suit of card, 1 is spade,
+     * 2 is heart, 3 is diamond, 4 is club
+     */
     public short getSuit()
     {
         short Suit = (short) (Id / 100);
         return Suit;
     }
 
+    /**
+     * @return the number of card, 10 is J, 11 is Q, 12 is K, 13 is A
+     */
     public short getNumber()
     {
         short Number = (short) (Id % 100);
