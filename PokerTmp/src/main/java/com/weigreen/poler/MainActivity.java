5b418fbfe28ce6568148693409df734d2940ce97
green <andy19310@gmail.com>
2013-06-10 19:27:55 +0800

    hihi

--- PokerTmp/src/main/java/com/weigreen/poler/MainActivity.java
+++ PokerTmp/src/main/java/com/weigreen/poler/MainActivity.java
@@ -1,5 +1,10 @@
 package com.weigreen.poler;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -7,25 +12,31 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private final String TAG = "BLUETOOTHT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Card[][] data = Functions.dealCard();
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+        /*Card[][] data = Functions.dealCard();
         for (int i=0; i<data.length; i++){
             for (int j=0; j<data[i].length; j++){
                 Log.d("Person" + i, String.valueOf(data[i][j].getSuit()) + String.format("%2d", data[i][j].getNumber()));
             }
-        }
+        }*/
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+
 }
