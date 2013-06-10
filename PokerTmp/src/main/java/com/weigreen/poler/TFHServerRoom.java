09f1fed504286040f7da7c1c8e3f4950f2d9215d
wind <aszx4510@yahoo.com.tw>
2013-06-10 19:32:24 +0800

    Merge branch 'master' of D:\workspaceAndroidStudio\PokerTmp with conflicts.

--- PokerTmp/src/main/java/com/weigreen/poler/TFHServerRoom.java
+++ PokerTmp/src/main/java/com/weigreen/poler/TFHServerRoom.java
@@ -1,168 +1,282 @@
+package com.weigreen.poler;
 
-import java.util.Random;
-import java.util.Timer;
+import android.util.Log;
 
-import com.weigreen.ncu.tfh.bridge.TFHBridgeMain;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
-public class TFHServerRoom implements Runnable
+/**
+ * Created by wind on 2013/6/7.
+ */
+public class TFHServerRoom
 {
-    private int peopleNumber = 0;
-    private int questionNumber;
-    private int roomPort;
-    private long[] playerList;
+    private final String LOG_TAG = "TFHServerRoom.java";
 
-    private int nowPlayingNumber;
+    private short pass = 0; // pass times
 
-    private final Random generator = new Random();
-    private Timer timer;
+    // godCard
+    private short callPlayerNumber; // the player number which call
+    private short godCardSuit = 0;
+    private short heap = 0;
 
-    //	private TMHServerRoomSecondTask secondTask;
-//	private TMHServerRoomDatabase database;
-    private TFHServerRoomListener server;
+    // play
+    private short part = 0;
+    private short initialCardSuit; // the first suit per stage
+    private short[] stageCardIds = new short[4];
+    private short initialPlayerNumber;
+    private short easternHeap = 0;
+    private short northernHeap = 0;
 
-    private TFHServerDatabase db;
 
-    public TFHServerRoom(int port, String roomName, TFHServerDatabase db)
+    // unfinished!!!!!!!!!!!
+    public TFHServerRoom(int port)
     {
-        server = new TFHServerRoomListener(port, this, db);
-        new Thread(server).start();
 
-        roomPort = port;
-        playerList = new long[3];
-        //database = new TMHServerRoomDatabase();
-
-        //timer = new Timer();
-        //secondTask = new TFHServerRoomSecondTask(database, this, port);
-
-        //timer.schedule(secondTask, 0, 1000);
     }
-
+/*
     public void haveNewData(TFHBridgeMain main)
     {
+        Log.d(LOG_TAG, "haveNewData()");
+
         short command = main.getCommand();
-        long userId = main.getUserId();
-		
-/*		if(command == TFHComm.ROOM_NEW_PLAYER)
-		{
-			
-			playerList[peopleNumber++] = userId;
-			System.out.println("P:"+peopleNumber);
-		}
-		
-		if(command == TFHComm.ROOM_DATA)
-		{
-			TMHBridgeDataPlayer playerData = (TMHBridgeDataPlayer) main.getData();
-			
-			int playerNumber = playerData.getPlayerNumber();
-			int playerAnswer = playerData.getPlayerAnswer();
-			
-			if(judge(playerAnswer)) // correct answer
-			{
-				int newPlayerNumber = changePlayerNumber(playerNumber);
-				nowPlayingNumber = newPlayerNumber;
-				int newQuestionNumber  = changeQuestionNumber(questionNumber);
-				questionNumber = newQuestionNumber;
-				
-				TMHBridgeDataRoom roomData = new TMHBridgeDataRoom("RIGHT", newPlayerNumber, newQuestionNumber, secondTask.getSeconds(), playerList);
-				TMHBridgeMain newMain = new TMHBridgeMain(TMHComm.ROOM_DATA, 1, roomData);
-				
-				server.sendToAll(newMain);
-			}
-			else // wrong answer
-			{
-				TMHBridgeDataRoom roomData = new TMHBridgeDataRoom("WRONG", playerNumber, questionNumber, secondTask.getSeconds(), playerList);
-				TMHBridgeMain newMain = new TMHBridgeMain(TMHComm.ROOM_DATA, 1, roomData);
-				
-				server.sendToAll(newMain);
-			}
-			
-		}
-		
-		
-		*/
-    } // end method haveNewData
 
-
-
-    /*	public boolean judge(int playerAnswer)
+        if(command == TFHComm.GOD_CARD_DATA)
         {
+            Log.d(LOG_TAG, "TFHComm.GOD_CARD_DATA");
 
+            TFHBridgeDataGodCard godCardData = (TFHBridgeDataGodCard) main.getData();
 
-            if (playerAnswer == database.getQuestionAnswer(questionNumber))
+            short newCallPlayerNumber = godCardData.getPlayerNumber();
+            short newGodCardSuit = godCardData.getGodCardSuit();
+            short newHeap = godCardData.getHeap();
+
+            if(newHeap == 0)
             {
-                return true;
+                Log.d(LOG_TAG, "Player passed");
+
+                pass++;
+
+                if(pass >= 3)
+                {
+                    Log.d(LOG_TAG, "triple pass, over to call god card");
+
+                    initialPlayerNumber = callPlayerNumber;
+
+                    TFHBridgeDataGodCard newGodCardData = new TFHBridgeDataGodCard("OVER", callPlayerNumber, godCardSuit, heap);
+                    TFHBridgeMain newMain = new TFHBridgeMain(TFHComm.GOD_CARD_DATA, newGodCardData);
+
+                    server.sendToAll(newMain);
+                }
+                else
+                {
+                    Log.d(LOG_TAG, "pass < 3");
+
+                    TFHBridgeDataGodCard newGodCardData = new TFHBridgeDataGodCard("KEEP", newCallPlayerNumber, newGodCardSuit, newHeap);
+                    TFHBridgeMain newMain = new TFHBridgeMain(TFHComm.GOD_CARD_DATA, newGodCardData);
+
+                    server.sendToAll(newMain);
+                }
             }
             else
             {
-                return false;
-            }
+                Log.d(LOG_TAG, "no pass, keep call");
 
+                pass = 0;
+
+                callPlayerNumber = newCallPlayerNumber;
+                godCardSuit = newGodCardSuit;
+                heap = newHeap;
+
+                TFHBridgeDataGodCard newGodCardData = new TFHBridgeDataGodCard("KEEP", callPlayerNumber, godCardSuit, heap);
+                TFHBridgeMain newMain = new TFHBridgeMain(TFHComm.GOD_CARD_DATA, newGodCardData);
+
+                server.sendToAll(newMain);
+            }
         }
 
 
-        public void start(int questionNumber, int second)
+        if(command == TFHComm.PLAYER_DATA)
         {
-            int initialPlyaerNumber = generator.nextInt(3);
-            nowPlayingNumber = initialPlyaerNumber;
+            Log.d(LOG_TAG, "TFHComm.PLAYER_DATA");
 
-            this.questionNumber = questionNumber;
+            TFHBridgeDataPlayer playerData = (TFHBridgeDataPlayer) main.getData();
 
-            TMHBridgeDataRoom roomData = new TMHBridgeDataRoom("START", initialPlyaerNumber, questionNumber, second, playerList);
-            TMHBridgeMain newMain = new TMHBridgeMain(TMHComm.ROOM_DATA, 1, roomData);
+            TFHBridgeDataRoom roomData = analyze(playerData);
+            TFHBridgeMain newMain = new TFHBridgeMain(TFHComm.ROOM_DATA, roomData);
 
             server.sendToAll(newMain);
         }
 
-
-        public void boom()
+        if(command == TFHComm.CARD_DATA)
         {
-            TMHBridgeDataRoom roomData = new TMHBridgeDataRoom("BOOM", nowPlayingNumber, 0, -100, playerList);
-            TMHBridgeMain newMain = new TMHBridgeMain(TMHComm.ROOM_DATA, 1, roomData);
+            Log.d(LOG_TAG, "TFHComm.CARD_DATA");
 
-            timer.cancel();
-            database.deleteRoom(roomPort);
+            TFHBridgeDataCard cardData = new TFHBridgeDataCard(Functions.dealCard());
+            TFHBridgeMain newMain = new TFHBridgeMain(TFHComm.CARD_DATA, cardData);
+
             server.sendToAll(newMain);
-            server.closeServer();
         }
+    }
+*/
 
-
-        public int changePlayerNumber(int playerNumber)
-        {
-            int newPlayerNumber = generator.nextInt(3);
-
-            while(playerNumber == newPlayerNumber)
-            {
-                newPlayerNumber = generator.nextInt(3);
-            }
-
-            return newPlayerNumber;
-        }
-
-        public int changeQuestionNumber(int questionNumber)
-        {
-            int questionAmount = database.getQuestionAmount();
-            int newQuestionNumber = generator.nextInt(questionAmount) + 1;
-
-            while(questionNumber == newQuestionNumber)
-            {
-                newQuestionNumber = generator.nextInt(questionAmount) + 1;
-            }
-
-            return newQuestionNumber;
-        }
-
-
-
-        public int getPeopleNumber()
-        {
-            return peopleNumber;
-        }
-
-        */
-    @Override
-    public void run()
+    private TFHBridgeDataRoom analyze(TFHBridgeDataPlayer playerData)
     {
+        Log.d(LOG_TAG, "analyze()");
 
+        short playerNumber = playerData.getPlayerNumber();
+        short cardId = playerData.getCardId();
+
+        if(++part == 1)
+        {
+            Log.d(LOG_TAG, "first player");
+
+            initialCardSuit = (short)(cardId / 100);
+            stageCardIds[playerNumber] = cardId;
+
+            TFHBridgeDataRoom roomData = new TFHBridgeDataRoom("KEEP", initialPlayerNumber, (short) ((initialPlayerNumber + part) % 4), cardId, easternHeap, northernHeap);
+            return roomData;
+        }
+        else if(part < 4)
+        {
+            Log.d(LOG_TAG, "other players");
+
+            stageCardIds[playerNumber] = cardId;
+
+            TFHBridgeDataRoom roomData = new TFHBridgeDataRoom("KEEP", initialPlayerNumber, (short) ((initialPlayerNumber + part) % 4), cardId, easternHeap, northernHeap);
+            return roomData;
+        }
+        else
+        {
+            Log.d(LOG_TAG, "forth player, start a new game");
+
+            part = 0;
+
+            stageCardIds[playerNumber] = cardId;
+
+            short winPlayerId = getWinPlayerNumber(stageCardIds);
+
+            if(winPlayerId == 0 || winPlayerId == 2)
+            {
+                Log.d(LOG_TAG, "east and west players WIN");
+
+                easternHeap++;
+            }
+            else
+            {
+                Log.d(LOG_TAG, "north and south players WIN");
+
+                northernHeap++;
+            }
+
+            TFHBridgeDataRoom roomData = new TFHBridgeDataRoom("START", winPlayerId, winPlayerId, cardId, easternHeap, northernHeap);
+
+            return roomData;
+        }
     }
 
+
+    private short getWinPlayerNumber(short[] stageCardIds)
+    {
+        Log.d(LOG_TAG, "getWinPlayerNumber()");
+
+        short[] cardSuit = new short[4];
+        short[] cardNumber = new short[4];
+
+        LinkedList<Short> godCardPlayerList = new LinkedList<Short>();
+        LinkedList<Short> normalCardPlayerList = new LinkedList<Short>();
+
+        Log.d(LOG_TAG, "get suits and numbers of cards");
+        for(int i = 0; i < 4; i++)
+        {
+            cardSuit[i] = (short)(stageCardIds[i] / 100);
+            cardNumber[i] = (short)(stageCardIds[i] % 100);
+
+            if(cardSuit[i] == godCardSuit)
+            {
+                godCardPlayerList.add((short)i);
+            }
+            else if(cardSuit[i] == initialCardSuit)
+            {
+                normalCardPlayerList.add((short)i);
+            }
+        }
+
+        if(!godCardPlayerList.isEmpty())
+        {
+            Log.d(LOG_TAG, "biggest card is god card");
+
+            if(godCardPlayerList.size() > 1)
+            {
+                ListIterator<Short> iterator = godCardPlayerList.listIterator();
+
+                List<Short> godCardNumbers = new ArrayList<Short>();
+
+                while(iterator.hasNext())
+                {
+                    Log.d(LOG_TAG, "godCardNumber: " + cardNumber[iterator.next()]);
+
+                    godCardNumbers.add(cardNumber[iterator.next()]);
+                }
+
+                short winCardId = (short)(godCardSuit * 100 + Collections.max(godCardNumbers));
+
+                Log.d(LOG_TAG, "winCardId is " + winCardId);
+
+                for(int i = 0; i < 4; i++)
+                {
+                    if(stageCardIds[i] == winCardId)
+                    {
+                        Log.d(LOG_TAG, "winner is " + i);
+
+                        return (short)i;
+                    }
+                }
+            }
+            else
+            {
+                return godCardPlayerList.get(0);
+            }
+        }
+        else
+        {
+            if(normalCardPlayerList.size() > 1)
+            {
+                ListIterator<Short> iterator = normalCardPlayerList.listIterator();
+
+                List<Short> normalCardNumbers = new ArrayList<Short>();
+
+                while(iterator.hasNext())
+                {
+                    Log.d(LOG_TAG, "normalCardNumber: " + cardNumber[iterator.next()]);
+
+                    normalCardNumbers.add(cardNumber[iterator.next()]);
+                }
+
+                short winCardId = (short)(initialCardSuit * 100 + Collections.max(normalCardNumbers));
+
+                Log.d(LOG_TAG, "winCardId is " + winCardId);
+
+                for(int i = 0; i < 4; i++)
+                {
+                    if(stageCardIds[i] == winCardId)
+                    {
+                        Log.d(LOG_TAG, "winner is " + i);
+
+                        return (short)i;
+                    }
+                }
+            }
+            else
+            {
+                return normalCardPlayerList.get(0);
+            }
+        }
+        Log.d(LOG_TAG, "error happened, return -1");
+
+        return -1; // error happened
+    }
 }
